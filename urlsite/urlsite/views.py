23fffa35d87072646c7d8a23d98fa43d54ca2f29
dashinya <dariagovorun51@gmail.com>
2023-11-29 01:13:34 +0300

    homework_3

---
+++ urlsite/urlsite/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+MENU = {"главная":"/", "пост":"post", "о блоге":"about"}
+
+def main_page(request):
+
+        title = "Главная страница"
+        data = {"menu":MENU, "title": title}
+        return render(request, "./index.html", context=data)
+
+def post_page(request):
+
+        title = "Пост"
+        data = {"menu":MENU, "title": title}
+        return render(request, "./post.html", context=data)
+
+def about_page(request):
+
+        title = "О блоге"
+        data = {"menu":MENU, "title": title}
+        return render(request, "./about.html", context=data)
